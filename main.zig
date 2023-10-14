fe2388dc70c8f712707693a5ee8c722001ca8a57
erfalielGarcia <fulielas@gmail.com>
2023-10-14 17:11:00 +0200

    first main : the basic test to see returned arguments

---
+++ main.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    std.debug.print("Arguments: {s}\n", .{args});
+}
